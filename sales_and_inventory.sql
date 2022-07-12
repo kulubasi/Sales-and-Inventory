d64df19b5e351db4373ce5a715ac729fd3e72958
kulubasi <61836873+kulubasi@users.noreply.github.com>
2022-07-12 17:26:36 +0300

    Updated database
    put up to date database

--- sales_and_inventory.sql
+++ sales_and_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2022 at 08:45 AM
+-- Generation Time: Jul 12, 2022 at 04:25 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `sales_and_inventory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `descri` varchar(50) NOT NULL,
+  `qtty` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`id`, `name`, `descri`, `qtty`) VALUES
+(1, 'paper', 'A4', 3),
+(2, 'Pens', 'Bic', 45),
+(3, 'Camera Film', 'Roller Film', 2),
+(4, 'Epson Printer Ink', '5 bottles of blue green and yellow', 5),
+(5, 'Envelopes', 'Small', 50),
+(6, 'Files', 'Red files', 10),
+(7, 'Box Files', 'Black green and red files', 3);
 
 -- --------------------------------------------------------
 
@@ -43,25 +69,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `fname`, `lname`, `username`, `tel`, `pswd`, `usertype`) VALUES
-(3, 'judei@gmail.com', 'laban', 'kulubasi', 'maximum', '0786508231', 'lloppl', 'Employer'),
-(4, 'em@gmail.com', 'emperor', 'haile', 'selasi', '0786199019', 'saile222', 'Desk officer'),
-(5, 'mayor@2022', 'mayur', 'ict', 'mayurict', '0789786789', '12345', 'Manager'),
-(6, 'finish@gmail.com', 'finisher', 'nsubuga', 'finisher22', '0786545654', 'finisher22', 'Store keeper'),
-(7, 'ooooo@gmail.com', 'oooo', 'oooo', 'oooo', '000000000000000', 'oooooooooooo', 'Admin'),
-(8, '000000000@gmail.com', 'jjj', 'jjjjjjjjjj', 'jjjjjjj', '0789786000', 'jjjjjjj', 'Admin'),
-(9, 'judei@gmail.com', 'jude', 'max', 'finisher22', '0786549099', 'lolo00', 'Store keeper'),
-(10, 'judei@gmail.com', 'jude', 'kulubasi', 'maximum', '0786545654', 'lll', 'Desk officer'),
-(11, 'judei@gmail.com', 'emperor', 'max', 'emperor', '999999999999999', '9999999999999', 'Manager'),
-(12, 'zoo@gmail.com', 'zoena', 'zuli', 'zuta', '098998987899', 'zulita', 'Store keeper'),
-(13, 'labankulubasi@gmail.com', 'jude', 'max', 'maximum', '0000000000000', '000000000000', 'Admin'),
-(14, 'zulu@gmail.com', 'zulu', 'tams', 'zulia', '0765654345', 'zulio', 'Manager'),
-(15, 'zulu@gmail.com', 'zulu', 'tams', 'zulia', '0765654345', 'ssss', 'Manager'),
-(16, 'labankulubasi@gmail.com', 'laban', 'kulubasi', 'kulubasi', '0786508231', 'kulubasi22', 'Store keeper'),
-(17, 'davie@gmail.com', 'david', 'kagimu', 'davie', '07654637211', 'davie22', 'Store keeper');
+(17, 'davie@gmail.com', 'david', 'kagimu', 'davie', '07654637211', 'davie22', 'Stock manager'),
+(19, 'manager@ictspecialistsug.com', 'manager', 'ict', 'manager', '0786564345', 'manager22', 'Manager'),
+(22, 'mark@gmail.com', 'mark', 'aine', 'mark', '0786432312', 'mark22', 'Stock Manager'),
+(23, 'ainembabazipraise48@gmail.com', 'praise', 'aine', 'aine', '0786452314', 'aine22', 'Desk officer');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -74,10 +95,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
